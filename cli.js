27c6d45547c69a27599751d345142faa3e487278
Paul Melnikow <email@paulmelnikow.com>
2017-02-21 10:03:15 -0500

    Update lint tooling

--- cli.js
+++ cli.js
@@ -10,8 +10,8 @@ const bins = argv._;
 const modes = ['all', 'some', 'first'].filter(mode => argv[mode]);
 
 if (! bins.length || modes.length > 1) {
-    console.error(usage);
-    process.exit(2);
+  console.error(usage);
+  process.exit(2);
 }
 
 const mode = modes.length ? modes[0] : 'all';
@@ -19,29 +19,29 @@ const mode = modes.length ? modes[0] : 'all';
 let fn;
 let printResult = false;
 switch (mode) {
-    case 'all': {
-        fn = require('hasbin').all.sync;
-        break;
-    }
+  case 'all': {
+    fn = require('hasbin').all.sync;
+    break;
+  }
 
-    case 'some': {
-        fn = require('hasbin').some.sync;
-        break;
-    }
+  case 'some': {
+    fn = require('hasbin').some.sync;
+    break;
+  }
 
-    case 'first': {
-        fn = require('hasbin').first.sync;
-        printResult = true;
-        break;
-    }
+  case 'first': {
+    fn = require('hasbin').first.sync;
+    printResult = true;
+    break;
+  }
 }
 
 const result = fn(bins);
 
 if (printResult) {
-    console.log(result);
+  console.log(result);
 }
 
 if (! result) {
-    process.exit(1);
+  process.exit(1);
 }
