9a0eeebdfd6ddaec900540f1d78bf563c93a8f66
Paul Melnikow <email@paulmelnikow.com>
2018-12-28 11:32:27 -0500

    Updates

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-hasbin-cli
-==========
+# hasbin-cli
 
 From the command line, check whether a binary exists in the PATH environment
 variable.
 
-
-Usage
------
+## Usage
 
 Make sure that one or more programs is installed.
 
@@ -27,31 +24,23 @@ Return the name of the first program that is installed.
 echo 'Your package manager is:' && hasbin --first brew apt-get
 ```
 
-
-Installation
-------------
+## Installation
 
 ```sh
 npm install hasbin-cli
 ```
 
-
-Contribute
-----------
+## Contribute
 
 - Issue Tracker: https://github.com/paulmelnikow/hasbin-cli/issues
 - Source Code: https://github.com/paulmelnikow/hasbin-cli
 
 Pull requests welcome!
 
-
-Support
--------
+## Support
 
 If you are having issues, please let me know.
 
-
-License
--------
+## License
 
 The project is licensed under the MIT license.
--- cli.js
+++ cli.js
@@ -1,47 +1,47 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict'
 
-const usage = "hasbin [--all|--some|--first] cmd [...]";
+const usage = 'hasbin [--all|--some|--first] cmd [...]'
 
-const argv = require("minimist")(process.argv.slice(2), { boolean: true });
+const argv = require('minimist')(process.argv.slice(2), { boolean: true })
 
-const bins = argv._;
-const modes = ["all", "some", "first"].filter(mode => argv[mode]);
+const bins = argv._
+const modes = ['all', 'some', 'first'].filter(mode => argv[mode])
 
 if (!bins.length || modes.length > 1) {
-  console.error(usage);
-  process.exit(2);
+  console.error(usage)
+  process.exit(2)
 }
 
-const mode = modes.length ? modes[0] : "all";
+const mode = modes.length ? modes[0] : 'all'
 
-let fn;
-let printResult = false;
+let fn
+let printResult = false
 switch (mode) {
-  case "all": {
-    fn = require("hasbin").all.sync;
-    break;
+  case 'all': {
+    fn = require('hasbin').all.sync
+    break
   }
 
-  case "some": {
-    fn = require("hasbin").some.sync;
-    break;
+  case 'some': {
+    fn = require('hasbin').some.sync
+    break
   }
 
-  case "first": {
-    fn = require("hasbin").first.sync;
-    printResult = true;
-    break;
+  case 'first': {
+    fn = require('hasbin').first.sync
+    printResult = true
+    break
   }
 }
 
-const result = fn(bins);
+const result = fn(bins)
 
 if (printResult) {
-  console.log(result);
+  console.log(result)
 }
 
 if (!result) {
-  process.exit(1);
+  process.exit(1)
 }
